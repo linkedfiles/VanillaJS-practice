0dab3e322c29c87c06373ec82d4d08cddb03d290
linkedfiles <dongyeop93@gmail.com>
2019-06-11 14:23:03 +0900

    js function practice

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-function sayHello(name, age) {
-    console.log('Hello', name, "you have", age, " years of age.");
+const calculator = {
+    plus: function(a, b){
+        return a + b;
+    },
+
+    minus: function(a, b){
+        return a - b;
+    },
+
+    divide: function(a, b){
+        return a / b;
+    },
+
+    multi: function(a, b){
+        return a * b;
+    },
+
+    power: function(a, b){
+        return a**b;
+    }
 }
 
+//console.log(greetJun);
+const plus = calculator.plus(5, 5)
+const minus = calculator.minus(5, 5)
+const divide = calculator.divide(5, 5)
+const multi = calculator.multi(5, 5)
+const power = calculator.power(5, 5)
 
-sayHello("Jun", 15);
+console.log(plus);
+console.log(minus);
+console.log(divide);
+console.log(multi);
+console.log(power);
+
